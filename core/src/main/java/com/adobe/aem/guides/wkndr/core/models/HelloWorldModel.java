76fdcb69346ca3ac44a865d9ef63c03b456bac76
Josh Mu <hello@joshmu.dev>
2021-02-09 08:55:36 +1000

    component basics complete

--- core/src/main/java/com/adobe/aem/guides/wkndr/core/models/HelloWorldModel.java
+++ core/src/main/java/com/adobe/aem/guides/wkndr/core/models/HelloWorldModel.java
@@ -19,11 +19,12 @@ import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_T
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
@@ -34,10 +35,10 @@ import com.day.cq.wcm.api.PageManager;
 
 import java.util.Optional;
 
-@Model(adaptables = Resource.class)
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class HelloWorldModel {
 
-    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
+    @ValueMapValue(name = PROPERTY_RESOURCE_TYPE)
     @Default(values="No resourceType")
     protected String resourceType;
 
@@ -49,6 +50,20 @@ public class HelloWorldModel {
     private ResourceResolver resourceResolver;
 
     private String message;
+
+    @ValueMapValue()
+    private String title;
+
+    @ValueMapValue
+    private String text;
+
+    public String getTitle() {
+        return StringUtils.isNotBlank(title) ? title : "Default title!";
+    }
+
+    public String getText() {
+        return StringUtils.isNotBlank(text) ? text.toUpperCase() : null;
+    }
 
     @PostConstruct
     protected void init() {
